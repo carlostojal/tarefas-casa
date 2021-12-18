91e0ecb85f060653260d392bf2b11f3f5776f22f
Carlos Tojal <carlos.tojal@hotmail.com>
2021-12-18 18:06:27 +0000

    first commit

---
+++ src/main/java/com/carlostojal/tarefascasa/TarefasCasaApplication.java
@@ -0,0 +1,13 @@
+package com.carlostojal.tarefascasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefasCasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefasCasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlostojal/tarefascasa/TarefasCasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlostojal.tarefascasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefasCasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
